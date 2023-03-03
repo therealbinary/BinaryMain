e0459af1af3edd75d6b84889fd89d1bfc9d636aa
binary <126812477+therealbinary@users.noreply.github.com>
2023-03-03 20:16:19 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,14 @@
+local PlaceId = game.PlaceId;
+local SupportedGames = {
+    [8540346411] = "Rebirth Champions X";
+}
+if SupportedGames[PlaceId] then 
+    local ScriptState = loadstring(game:HttpGet("https://raw.githubusercontent.com/therealbinary/BinaryBackend/main/Status.lua", true))()
+    if ScriptState[PlaceId] then
+       if ScriptState[PlaceId].ScriptDown == true then game.Players.LocalPlayer:Kick("binary | script down"); while true do end end;
+       local scs, err = pcall(function()
+          loadstring(game:HttpGet("https://github.com/therealbinary/BinaryGames/blob/main/"..PlaceId..".lua", true))()
+       end)
+       if scs then print("successfully loaded binary!") elseif err then game.Players.LocalPlayer:Kick("binary | script caught an error"); while true do end; end;
+    end
+end 
