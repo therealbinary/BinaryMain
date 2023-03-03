49fd3af98b82f27914d83f91288914a9e89326d5
binary <126812477+therealbinary@users.noreply.github.com>
2023-03-03 20:36:35 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,15 @@
 local PlaceId = game.PlaceId;
+getgenv().binary = loadstring(game:HttpGet("https://raw.githubusercontent.com/therealbinary/BinaryBackend/main/Functions.lua", true))()
 local SupportedGames = {
     [8540346411] = "Rebirth Champions X";
 }
 if SupportedGames[PlaceId] then 
     local ScriptState = loadstring(game:HttpGet("https://raw.githubusercontent.com/therealbinary/BinaryBackend/main/Status.lua", true))()
     if ScriptState[PlaceId] then
-       if ScriptState[PlaceId].ScriptDown == true then game.Players.LocalPlayer:Kick("binary | script down"); wait(3) game:Shutdown(); end;
+       if ScriptState[PlaceId].ScriptDown == true then binary.Notify({"binary | error", "script is down, wait for developer to restore its functionality", 4}); end;
        local scs, err = pcall(function()
           loadstring(game:HttpGet("https://raw.githubusercontent.com/therealbinary/BinaryGames/main/"..PlaceId..".lua", true))()
        end)
-       if scs then print("successfully loaded binary!") elseif err then game.Players.LocalPlayer:Kick("binary | script caught an error"); wait(3) game:Shutdown(); end;
+       if scs then binary.Notify({"binary | success", "script is being loaded, hang on, this shouldn't take more than 3 seconds", 4}) elseif err then binary.Notify({"binary | error", "script caught an error, report this issue and wait for developer to update the script", 4});end;
     end
 end 
