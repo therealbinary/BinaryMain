801b4f4e4da93398423c2df64cfd5f21ae350858
binary <126812477+therealbinary@users.noreply.github.com>
2023-03-03 20:17:50 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,10 +5,10 @@ local SupportedGames = {
 if SupportedGames[PlaceId] then 
     local ScriptState = loadstring(game:HttpGet("https://raw.githubusercontent.com/therealbinary/BinaryBackend/main/Status.lua", true))()
     if ScriptState[PlaceId] then
-       if ScriptState[PlaceId].ScriptDown == true then game.Players.LocalPlayer:Kick("binary | script down"); while true do end end;
+       if ScriptState[PlaceId].ScriptDown == true then game.Players.LocalPlayer:Kick("binary | script down"); wait(3) game:Shutdown(); end;
        local scs, err = pcall(function()
           loadstring(game:HttpGet("https://github.com/therealbinary/BinaryGames/blob/main/"..PlaceId..".lua", true))()
        end)
-       if scs then print("successfully loaded binary!") elseif err then game.Players.LocalPlayer:Kick("binary | script caught an error"); while true do end; end;
+       if scs then print("successfully loaded binary!") elseif err then game.Players.LocalPlayer:Kick("binary | script caught an error"); wait(3) game:Shutdown(); end;
     end
 end 
