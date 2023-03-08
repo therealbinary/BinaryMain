64d17adc19d6a09e615d45267efd73fdeec40793
binary <126812477+therealbinary@users.noreply.github.com>
2023-03-09 00:10:56 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,19 +1,16 @@
 local PlaceId = game.PlaceId;
-getgenv().binary = loadstring(game:HttpGet("https://raw.githubusercontent.com/therealbinary/BinaryBackend/main/Functions.lua", true))()
+local f = loadstring(game:HttpGet("https://raw.githubusercontent.com/therealbinary/BinaryBackend/main/Functions.lua", true))()
 local Lists = loadstring(game:HttpGet("https://raw.githubusercontent.com/therealbinary/BinaryBackend/main/WBList.lua", true))()
 if (Lists.Blacklist[game.Players.LocalPlayer.UserId]) then binary.Notify("binary | information", "user has been blacklisted from binary by its developer", 10); return; end;
-local SupportedGames = {
-    [8540346411] = "Rebirth Champions X",
-    [12192552089] = "Fruit Warriors",
-}
-if SupportedGames[PlaceId] then 
-    local ScriptState = loadstring(game:HttpGet("https://raw.githubusercontent.com/therealbinary/BinaryBackend/main/Status.lua", true))()
-    if ScriptState[PlaceId] then
-       if ScriptState[PlaceId].ScriptDown == true then binary.Notify("binary | error", "script is down, wait for developer to restore its functionality", 4); end;
-       local scs, err = pcall(function()
+local ScriptState = loadstring(game:HttpGet("https://raw.githubusercontent.com/therealbinary/BinaryBackend/main/Status.lua", true))()
+if ScriptState[PlaceId] then 
+    if ScriptState[PlaceId].ScriptDown == true then f.Notify("binary | error", "script is down, wait for developer to restore its functionality", 4); end;
+        local scs, err = pcall(function()
           local vu = game:GetService("VirtualUser")game:GetService("Players").LocalPlayer.Idled:connect(function()   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)   wait(1)   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)end)
           loadstring(game:HttpGet("https://raw.githubusercontent.com/therealbinary/BinaryGames/main/"..PlaceId..".lua", true))()
-       end)
-       if scs then binary.Notify("binary | success", "script is being loaded, hang on, this shouldn't take more than 3 seconds", 4) elseif err then binary.Notify("binary | error", "script caught an error, report this issue and wait for developer to update the script", 4);end;
+        end)
+        if scs then f.Notify("binary | success", "script is being loaded, hang on, this shouldn't take more than 3 seconds", 4) elseif err then binary.Notify("binary | error", "script caught an error, report this issue and wait for developer to update the script", 4);end;
     end
-end 
+else
+    f.Notify("binary | error", "game is not yet supported by binary", 4)
+end
